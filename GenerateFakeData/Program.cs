c5d290e672e4906b999d59e8428c422d3ec9fbde
mstrucka <mstrucka@gmail.com>
2022-03-19 09:32:44 +0100

    door and floor added, refactoring

--- GenerateFakeData/Program.cs
+++ GenerateFakeData/Program.cs
@@ -1,15 +1,23 @@
 ﻿using GenerateFakeData.data;
+using System;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-GenerateFakeData.data.NameGenerator haha = new NameGenerator();
-GenerateFakeData.data.Cpr cpr = new Cpr();
-System.Console.WriteLine(haha.generateRandomPhoneNumber());
-System.Console.WriteLine(cpr.generateCprNumber(cpr.DoB,true));
-System.Console.WriteLine(cpr.DoB);
-System.Console.WriteLine(Address.Street(7));
-System.Console.WriteLine(Address.Number());
+NameGenerator haha = new NameGenerator();
+Cpr cpr = new Cpr();
+Console.WriteLine(haha.generateRandomPhoneNumber());
+Console.WriteLine(cpr.generateCprNumber(cpr.DoB,true));
+Console.WriteLine(cpr.DoB);
+Console.WriteLine(Address.Street());
+Console.WriteLine(Address.Number());
+Console.WriteLine(Address.Floor());
+Console.WriteLine(Address.Door());
+
+// testing for cycle if bulk test is needed
+for (int i = 0; i < 100; i++)
+{
+}
 
 NameGenderGenerator nameGender = new NameGenderGenerator();
 if (nameGender.GetRandomPerson(out string firstName, out string lastName, out string gender))
--- GenerateFakeData/README.md
+++ GenerateFakeData/README.md
@@ -4,9 +4,10 @@ Testing mandatory assignment
 TODO:
 - [ ] create db with Addresses
     - [ ] create layer for DB communication
-- [ ] extract random names from person-names.json
-- [ ] generate CPRs
-    - [ ] match DOB with CPR
+- [ ] extract random names from person-names.json DONE
+- [ ] generate CPRs DONE
+    - [ ] match DOB with CPR DONE
 - [ ] generate phone number - Marek DONE
+- [ ] generate full address - missing read from DB for postal codes
   
  
