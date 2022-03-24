3c72b7aa02cb57af6534712866bea8eebdc17422
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-03-24 11:09:10 +0100

    Testing

---
+++ TestingGenerateFakeData/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingGenerateFakeData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("TestingGenerateFakeData")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d237607-74ec-4e11-99f8-ab58affb705d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingProjectGenerateFakeData/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TestingProjectGenerateFakeData
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
