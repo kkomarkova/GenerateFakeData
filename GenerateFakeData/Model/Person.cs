ebf1c4f25e6a4321c6c5da4d80170b22f07933cc
Sam <sam.horacek@gmail.com>
2022-03-25 13:44:54 +0100

    Refactor of phone number generation

--- GenerateFakeData/Model/Person.cs
+++ GenerateFakeData/Model/Person.cs
@@ -6,10 +6,6 @@ namespace GenerateFakeData.Model
 {
     public class Person
     {        
-        int[] starters = new int[] {2, 30, 31, 40, 41, 42, 50, 51, 52, 53, 60, 61, 71, 81, 91, 92, 93,
-            342, 344, 345, 346, 347, 348, 349, 356, 357, 359, 362, 365, 366, 389, 398, 431, 441, 462, 466,  468,  472,  474,  476,  478,  485,
-            486,  488, 489,  493, 494, 495, 496,  498, 499,  542, 543,  545,  551, 552, 556, 571, 572, 573, 574, 577, 579, 584, 586, 587, 589,
-            597, 598, 627, 629, 641, 649, 658, 662, 663, 664, 665, 667, 692, 693, 694, 697, 771, 772, 782, 783, 785, 786, 788, 789, 826, 827,829};
         public string FullName { get; set; }
         // TODO: change to Enum
         public string Gender { get; set; }
@@ -88,26 +84,8 @@ namespace GenerateFakeData.Model
 
         public void SetRandomPhoneNumber()
         {
-            string Generated = "";
-            Random Rnd = new Random();
-            int NumberLength = 8;
-            //Pick one index from the array, from 0 to the length of the starters
-            int StartingSequence = starters[Rnd.Next(0, starters.Length)];
-            //How many digits are in one object
-            int LengthOfStartingSequence = StartingSequence.ToString().Length;
-            Generated += StartingSequence.ToString();
-            //Generate random of the digit missing from number length - length of starting sequence (1,2,3)
-            for (int i = 0; i < NumberLength - LengthOfStartingSequence; i++)
-            {
-                Generated += Rnd.Next(0, 10);
-            }
-            ValidatePhoneNumber(Generated);
-            PhoneNumber = Generated;
-        }
-        //Validator of Phone number
-        public bool ValidatePhoneNumber(string numberToTest)
-        {
-            return starters.Any(x => numberToTest.StartsWith(x.ToString())) && (numberToTest.Length == 8);
+            PhoneNoService phoneNumberGenerator = new();
+            PhoneNumber = phoneNumberGenerator.GenerateRandomPhoneNumber();
         }
         public string SetStreet()
         {
--- TestingProjectGenerateFakeData/HelperTests.cs
+++ TestingProjectGenerateFakeData/HelperTests.cs
@@ -10,7 +10,7 @@ namespace TestingProjectGenerateFakeData
 {
     public class HelperTests
     {
-        Person person = new Person();
+        PhoneNoService phoneGenerator = new();
 
 #region Validate PhoneNumber Validator
 
@@ -19,35 +19,35 @@ namespace TestingProjectGenerateFakeData
         public void IfValidPhoneNumber_ReturnTrue()
         {   
             string validNumberToTest = "22222222";
-            bool isValid = person.ValidatePhoneNumber(validNumberToTest);
+            bool isValid = phoneGenerator.ValidatePhoneNumber(validNumberToTest);
             Assert.True(isValid);
         }
         [Fact]
         public void IfValidPhoneNumber_ReturnFalseBecauseEmptyString()
         {
             string validNumberToTest = "";
-            bool isValid = person.ValidatePhoneNumber(validNumberToTest);
+            bool isValid = phoneGenerator.ValidatePhoneNumber(validNumberToTest);
             Assert.False(isValid);
         }
         [Fact]
         public void IfValidPhoneNumber_ReturnFalseBecauseInvalidString()
         {
             string validNumberToTest = "xxx";
-            bool isValid = person.ValidatePhoneNumber(validNumberToTest);
+            bool isValid = phoneGenerator.ValidatePhoneNumber(validNumberToTest);
             Assert.False(isValid);
         }
         [Fact]
         public void IfValidPhoneNumber_ReturnFalseBecauseInvalidStartingNumber()
         {
             string validNumberToTest = "99999999";
-            bool isValid = person.ValidatePhoneNumber(validNumberToTest);
+            bool isValid = phoneGenerator.ValidatePhoneNumber(validNumberToTest);
             Assert.False(isValid);
         }
         [Fact]
         public void IfValidPhoneNumber_ReturnFalseBecauseInvalidLength()
         {
             string validNumberToTest = "222222222"; //valid starter, 9 numbers
-            bool isValid = person.ValidatePhoneNumber(validNumberToTest);
+            bool isValid = phoneGenerator.ValidatePhoneNumber(validNumberToTest);
             Assert.False(isValid);
         }
         #endregion
