2a199f560dda7f3b363ab59696f5cfc40c3be97e
Sam <sam.horacek@gmail.com>
2022-03-28 10:38:09 +0200

    Updated CPR integration test

--- TestingProjectGenerateFakeData/integration/CprTests.cs
+++ TestingProjectGenerateFakeData/integration/CprTests.cs
@@ -1,35 +1,38 @@
 ﻿using System;
 using GenerateFakeData.Model;
+using GenerateFakeData.Service;
 using Xunit;
 
 namespace TestingProjectGenerateFakeData.integration
 {
     public class CprTests
     {
-        [Fact]
-        public void TestIfFemaleCprEven()
+        [Theory]
+        [InlineData(Gender.Male, 1)]
+        [InlineData(Gender.Female, 0)]
+        public void TestIfCprLastDigitCorrect(Gender gender, int remainderAfterDivision)
         {
             //Arrange
             Person person = new Person();
             //Act
-            person.Gender = Gender.Female;
+            person.Gender = gender;
             person.GenerateCprNumber();
             var cpr = Convert.ToInt64(person.CprNumber);
             //Assert
-            Assert.True(cpr % 2 == 0);
+            Assert.True(cpr % 2 == remainderAfterDivision);
         }
-
         [Fact]
-        public void TestIfMaleCprUneven()
+        public void TestIfCprGenerationSuccessful()
         {
             //Arrange
             Person person = new Person();
+            CprService cprValidator = new CprService();
             //Act
-            person.Gender = Gender.Male;
             person.GenerateCprNumber();
-            var cpr = Convert.ToInt64(person.CprNumber);
+            var isCprValid = cprValidator.ValidateCpr(person.Gender, 
+                person.CprNumber, person.DateOfBirth);
             //Assert
-            Assert.True(cpr % 2 == 1);
+            Assert.True(isCprValid);
         }
     }
 }
